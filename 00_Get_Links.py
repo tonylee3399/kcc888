a666b49fae8e5793f986b312452c927b08d564b0
Tole <tole.tonylee90@gmail.com>
2018-07-18 22:27:49 +0700

    Exceptions added to 00_* files
    Added exception handling to be logged into 00_*.py files
    
    Signed-off-by: Tole <tole.tonylee90@gmail.com>

--- 00_Get_Links.py
+++ 00_Get_Links.py
@@ -86,64 +86,66 @@ LDEBUG(" ".join(["News Save Path:", COMPANY_NEWS_SAVE_PATH]))
 LDEBUG(" ".join(["Announcement Save Path:", COMPANY_ANNOUNCEMENT_SAVE_PATH]))
 
 # ==================== Main Process ====================
+try:
+    # 1. Requesting HTML GET
+    LINFO("Requesting page {}".format(URL))
+    page = requests.get(URL)
+    LINFO("Page returns <{}> code".format(page.status_code))
 
-# 1. Requesting HTML GET
-LINFO("Requesting page {}".format(URL))
-page = requests.get(URL)
-LINFO("Page returns <{}> code".format(page.status_code))
+    if page.status_code == 200:
+        # 2. Feeding HTML contents into BeautifulSoup object
+        LINFO("Souping page content")
+        soup = BeautifulSoup(page.content, 'html5lib')
+        LINFO("Finished souping page content ")
 
-if page.status_code == 200:
-    # 2. Feeding HTML contents into BeautifulSoup object
-    LINFO("Souping page content")
-    soup = BeautifulSoup(page.content, 'html5lib')
-    LINFO("Finished souping page content ")
+        # 3. Extracting targeted information
+        LINFO("Finding target information")
+        target_data = soup.find_all('td', {'class':TARGET_DATA_ATTRIBUTE})
+        LINFO("Length of the target data: {}".format(len(target_data)))
+        if len(target_data) > 0:
+            # 4. If target_data is found, start main algorithm
+            LINFO("Target data is not empty. Starting algorithm")
+            cmp_info_links = {}             # Creating links for retrieving Company INFO
+            cmp_news_links = {}             # Creating links for retrieving Company NEWS
+            cmp_announcement_links = {}     # Creating links for retrieving Company ANNOUNCEMENT
+            
+            # ==================== Main Algorithm ====================
+            LINFO("Iterating through all found links...")
+            for i, d in enumerate(target_data):
+                if (i+1) % 10 == 0:
+                    LINFO("Iteration {}...".format(i+1))
+                big5_link = d.contents[0]['href'].split('=')[-1].encode('big5')     # Extract the webpage within 'href' tag
+                cmp_info_links[i+1] = COMPANY_INFO_PRECEDING_URL + urllib.quote_plus(big5_link)     # Process and convert URL to big5 format
+                cmp_news_links[i+1] = COMPANY_NEWS_PRECEDING_URL + urllib.quote_plus(big5_link)     # Process and convert URL to big5 format
+                cmp_announcement_links[i+1] = COMPANY_ANNOUNCEMENT_PRECEDING_URL + urllib.quote_plus(big5_link) # Process and convert URL to big5 format
+                LDEBUG("{}. ".format(i+1) + " -> ".join([big5_link.decode('big5').encode('utf8'), urllib.quote_plus(big5_link)]))
+            LINFO("Finish iterating through all found links")
+            # ==================== Algorithm Finish ====================
 
-    # 3. Extracting targeted information
-    LINFO("Finding target information")
-    target_data = soup.find_all('td', {'class':TARGET_DATA_ATTRIBUTE})
-    LINFO("Length of the target data: {}".format(len(target_data)))
-    if len(target_data) > 0:
-        # 4. If target_data is found, start main algorithm
-        LINFO("Target data is not empty. Starting algorithm")
-        cmp_info_links = {}             # Creating links for retrieving Company INFO
-        cmp_news_links = {}             # Creating links for retrieving Company NEWS
-        cmp_announcement_links = {}     # Creating links for retrieving Company ANNOUNCEMENT
-        
-        # ==================== Main Algorithm ====================
-        LINFO("Iterating through all found links...")
-        for i, d in enumerate(target_data):
-            if (i+1) % 10 == 0:
-                LINFO("Iteration {}...".format(i+1))
-            big5_link = d.contents[0]['href'].split('=')[-1].encode('big5')     # Extract the webpage within 'href' tag
-            cmp_info_links[i+1] = COMPANY_INFO_PRECEDING_URL + urllib.quote_plus(big5_link)     # Process and convert URL to big5 format
-            cmp_news_links[i+1] = COMPANY_NEWS_PRECEDING_URL + urllib.quote_plus(big5_link)     # Process and convert URL to big5 format
-            cmp_announcement_links[i+1] = COMPANY_ANNOUNCEMENT_PRECEDING_URL + urllib.quote_plus(big5_link) # Process and convert URL to big5 format
-            LDEBUG("{}. ".format(i+1) + " -> ".join([big5_link.decode('big5').encode('utf8'), urllib.quote_plus(big5_link)]))
-        LINFO("Finish iterating through all found links")
-        # ==================== Algorithm Finish ====================
+            # ==================== Information Export ====================
+            LINFO("Saving captured INFO links into {}".format(COMPANY_INFO_SAVE_PATH))
+            with open(COMPANY_INFO_SAVE_PATH, 'w') as fp:
+                json.dump(cmp_info_links, fp, indent=4, sort_keys=True)
+            LINFO("Saved all the INFO links into {}".format(COMPANY_INFO_SAVE_PATH))
 
-        # ==================== Information Export ====================
-        LINFO("Saving captured INFO links into {}".format(COMPANY_INFO_SAVE_PATH))
-        with open(COMPANY_INFO_SAVE_PATH, 'w') as fp:
-            json.dump(cmp_info_links, fp, indent=4, sort_keys=True)
-        LINFO("Saved all the INFO links into {}".format(COMPANY_INFO_SAVE_PATH))
+            LINFO("Saving captured NEWS links into {}".format(COMPANY_NEWS_SAVE_PATH))
+            with open(COMPANY_NEWS_SAVE_PATH, 'w') as fp:
+                json.dump(cmp_news_links, fp, indent=4, sort_keys=True)
+            LINFO("Saved all the NEWS links into {}".format(COMPANY_NEWS_SAVE_PATH))
 
-        LINFO("Saving captured NEWS links into {}".format(COMPANY_NEWS_SAVE_PATH))
-        with open(COMPANY_NEWS_SAVE_PATH, 'w') as fp:
-            json.dump(cmp_news_links, fp, indent=4, sort_keys=True)
-        LINFO("Saved all the NEWS links into {}".format(COMPANY_NEWS_SAVE_PATH))
-
-        LINFO("Saving captured ANNOUNCEMENT links into {}".format(COMPANY_ANNOUNCEMENT_SAVE_PATH))
-        with open(COMPANY_ANNOUNCEMENT_SAVE_PATH, 'w') as fp:
-            json.dump(cmp_announcement_links, fp, indent=4, sort_keys=True)
-        LINFO("Saved all the ANNOUNCEMENT links into {}".format(COMPANY_ANNOUNCEMENT_SAVE_PATH))
-        # ==================== End of Export ====================
-
-        LINFO("Script finished\n\n")
+            LINFO("Saving captured ANNOUNCEMENT links into {}".format(COMPANY_ANNOUNCEMENT_SAVE_PATH))
+            with open(COMPANY_ANNOUNCEMENT_SAVE_PATH, 'w') as fp:
+                json.dump(cmp_announcement_links, fp, indent=4, sort_keys=True)
+            LINFO("Saved all the ANNOUNCEMENT links into {}".format(COMPANY_ANNOUNCEMENT_SAVE_PATH))
+            # ==================== End of Export ====================
+            
+            LINFO("Script finished\n\n")
+        else:
+            LERROR("Data not found! Please fix the following code:")
+            LERROR("\ttarget_data = soup.find_all('td', {'class':TARGET_DATA_ATTRIBUTE})")
     else:
-        LERROR("Data not found! Please fix the following code:")
-        LERROR("\ttarget_data = soup.find_all('td', {'class':TARGET_DATA_ATTRIBUTE})")
-else:
-    LERROR("The page returns a '{}' status code. Please check".format(page.status_code))
-
-LINFO("Script finished")
+        LERROR("The page returns a '{}' status code. Please check".format(page.status_code))
+except Exception as e:
+    LERROR("'{}' exception was raised!".format(type(e)))
+    LERROR("Message: {}".format(' - '.join(e.args)))
+    LWARNING("Script finished with exception(s)!")
