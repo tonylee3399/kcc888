6802bd7468e2ec2539eabee4bbff00982c917578
Tole <tole.tonylee90@gmail.com>
2018-07-18 22:32:29 +0700

    Added exception handler to 05 file

--- 05_Wrappers.py
+++ 05_Wrappers.py
@@ -37,46 +37,51 @@ LWARNING = lambda s: logger.warning(s)
 # ============================================================
 
 if __name__ == "__main__":
-    # ==================== Resource Generation ====================
-    start_time = time.time()
-    LINFO("Generating company name parser")
-    generate = subprocess.Popen("python {}".format(join(SCRIPT_ROOT_FOLDER, "00_generate_company_name_parser.py")), shell=True)
-    LINFO("Generating target links")
-    get_link = subprocess.Popen("python {}".format(join(SCRIPT_ROOT_FOLDER, "00_Get_Links.py")), shell=True)
-    LINFO("Wait for resource generation to complete")
-    exit_codes = [p.wait() for p in generate, get_link]
-    # ============================================================
+    try:
+        # ==================== Resource Generation ====================
+        start_time = time.time()
+        LINFO("Generating company name parser")
+        generate = subprocess.Popen("python {}".format(join(SCRIPT_ROOT_FOLDER, "00_generate_company_name_parser.py")), shell=True)
+        LINFO("Generating target links")
+        get_link = subprocess.Popen("python {}".format(join(SCRIPT_ROOT_FOLDER, "00_Get_Links.py")), shell=True)
+        LINFO("Wait for resource generation to complete")
+        exit_codes = [p.wait() for p in generate, get_link]
+        # ============================================================
 
 
-    # ==================== Scraping Process Start ====================
-    LINFO("Running multiple processes simultaneously")
+        # ==================== Scraping Process Start ====================
+        LINFO("Running multiple processes simultaneously")
 
-    LINFO("Running company info scraping")    
-    info = subprocess.Popen("python {}".format(join(SCRIPT_ROOT_FOLDER, "01_Company_Info_Scraper.py")), shell=True)
+        LINFO("Running company info scraping")    
+        info = subprocess.Popen("python {}".format(join(SCRIPT_ROOT_FOLDER, "01_Company_Info_Scraper.py")), shell=True)
 
-    LINFO("Running company news scraping")
-    news = subprocess.Popen("python {}".format(join(SCRIPT_ROOT_FOLDER, "02_Company_News_Scraping.py")), shell=True)
+        LINFO("Running company news scraping")
+        news = subprocess.Popen("python {}".format(join(SCRIPT_ROOT_FOLDER, "02_Company_News_Scraping.py")), shell=True)
 
-    LINFO("Running company announcement scraping")
-    announcement = subprocess.Popen("python {}".format(join(SCRIPT_ROOT_FOLDER, "03_Company_Announcement_Scraping.py")), shell=True)
+        LINFO("Running company announcement scraping")
+        announcement = subprocess.Popen("python {}".format(join(SCRIPT_ROOT_FOLDER, "03_Company_Announcement_Scraping.py")), shell=True)
 
-    LINFO("Now waiting for info, news, announcement scraping to finish..")
-    exit_codes = [p.wait() for p in info, news, announcement]
-    # =================================================================
+        LINFO("Now waiting for info, news, announcement scraping to finish..")
+        exit_codes = [p.wait() for p in info, news, announcement]
+        # =================================================================
 
 
-    # ==================== DB Operation Start ====================
-    LINFO("Running DB Operation in 10 seconds")
-    for _n in range(10, 0, -1):
-        print("{} ".format(_n)),
-    print("")
-    LINFO("Running DB Operation...")
+        # ==================== DB Operation Start ====================
+        LINFO("Running DB Operation in 10 seconds")
+        for _n in range(10, 0, -1):
+            print("{} ".format(_n)),
+        print("")
+        LINFO("Running DB Operation...")
 
-    # Uncomment the following 3 LOC for release
-    # db = subprocess.Popen("python {}".format(join(SCRIPT_ROOT_FOLDER, "04_DB_Operations.py")), shell=True)
-    # LINFO("Now waiting for DB Operations to finish..")
-    # exit_codes = db.wait()
-    # ============================================================
+        # Uncomment the following 3 LOC for release
+        # db = subprocess.Popen("python {}".format(join(SCRIPT_ROOT_FOLDER, "04_DB_Operations.py")), shell=True)
+        # LINFO("Now waiting for DB Operations to finish..")
+        # exit_codes = db.wait()
+        # ============================================================
 
-    LINFO("Terminating DB Operation process")
-    LINFO("Processes finished within: {:.3f}".format(time.time() - start_time))
+        LINFO("Terminating DB Operation process")
+        LINFO("Processes finished within: {:.3f}".format(time.time() - start_time))
+    except Exception as e:
+        LERROR("'{}' exception was raised!".format(type(e)))
+        LERROR("Message: {}".format(' - '.join(e.args)))
+        LWARNING("Script finished with exception(s)!")
